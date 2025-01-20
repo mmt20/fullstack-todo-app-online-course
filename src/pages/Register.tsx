d2f5b8c0efa381a1085a2a6d81ade72f6201c32c
mmt20 <mmt202002@gmail.com>
2025-01-21 00:49:24 +0200

    Add React Hook Form for registration page

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,16 +1,27 @@
 import Button from "../components/ui/Button";
 import Input from "../components/ui/Input";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+interface IFormInput {
+  username: string;
+  email: string;
+  password: string;
+}
 
 const RegisterPage = () => {
+  const { register, handleSubmit } = useForm<IFormInput>();
+  const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data);
   return (
     <div className="max-w-md mx-auto">
-      <h2 className="text-center mb-4 text-3xl font-semibold">Register to get access!</h2>
-      <form className="space-y-4">
-        <Input placeholder="Username" />
-        <Input placeholder="Email address" />
-        <Input placeholder="Password" />
+      <h2 className="text-center mb-4 text-3xl font-semibold">
+        Register to get access!
+      </h2>
+      <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
+        <Input placeholder="Username" {...register("username")} />
+        <Input placeholder="Email address" {...register("email")} />
+        <Input placeholder="Password" {...register("password")} />
 
-        <Button fullWidth>Login</Button>
+        <Button fullWidth>Register </Button>
       </form>
     </div>
   );
